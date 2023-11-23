0abf5a829b4e7741369354ea5c850a2030ea85e4
SingCow <75522316+FivePesos@users.noreply.github.com>
2023-11-23 12:12:43 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,45 +2,45 @@
 #include <stdlib.h>
 #define FULL_DEGREE 360
 
-void quadrant(short int angle);
+void quadrant(double angle);
 
 int main()
 {
-    short int angle;
+    double angle;
     printf("What is the angle: ");
-    scanf("%hd", &angle);
+    scanf("%lf", &angle);
 
     quadrant(angle);
     return 0;
 }
 
-void quadrant(short int angle){
+void quadrant(double angle){
 
-    if(angle == 0){
+    if(angle == 0.0){
         printf("\nIt is not in a quadrant but lies on the positive X-axis");
-    }else if( 0 < angle && angle < 90 ){
+    }else if( 0.0 < angle && angle < 90.0 ){
         printf("\nIt lies on the quadrant 1");
-    }else if(angle == 90){
+    }else if(angle == 90.0){
         printf("\nIt lies on the positive Y-axis");
-    }else if(90 < angle && angle < 180){
+    }else if(90.0 < angle && angle < 180.0){
         printf("\nIt lies on the quadrant 2");
-    }else if(angle == 180){
+    }else if(angle == 180.0){
         printf("\nIt lies on the negative X-axis");
-    }else if(180 < angle && angle < 270){
+    }else if(180.0 < angle && angle < 270.0){
         printf("\nIt lies on the quadrant 3");
-    }else if(angle == 270){
+    }else if(angle == 270.0){
         printf("\nIt lies on the negative Y-axis");
-    }else if(270 < angle && angle <= 360){
+    }else if(270.0 < angle && angle <= 360.0){
         printf("\nIt lies on the quadrant 4");
     }else{
-        angle %= FULL_DEGREE;
-        if( 0 < angle && angle < 90 ){
+        angle /= FULL_DEGREE;
+        if( 0.0 < angle && angle < 90.0 ){
             printf("\nIt lies on the quadrant 1");
-        }else if(90 < angle && angle < 180){
+        }else if(90.0 < angle && angle < 180.0){
             printf("\nIt lies on the quadrant 2");
-        }else if(180 < angle && angle < 270){
+        }else if(180.0 < angle && angle < 270.0){
             printf("\nIt lies on the quadrant 3");
-        }else if(270 < angle && angle <= 360){
+        }else if(270.0 < angle && angle <= 360.0){
             printf("\nIt lies on the quadrant 4");
         }
     }
